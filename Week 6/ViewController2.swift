5fe43f08323ca637b1eb7e19377b8581a5843287
Joakim Thorum Poulsen <43633549+JThorumP@users.noreply.github.com>
2020-02-14 08:45:07 +0100

    Add files via upload

---
+++ Week 6/ViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  HelloWorld
+//
+//  Created by Joakim Poulsen on 07/02/2020.
+//  Copyright © 2020 Joakim Poulsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    extension ViewController{
+        @IBAction func cancelToViewController(_ segue: UIStoryboard){
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Week 6/ViewController3.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController3.swift
+//  HelloWorld
+//
+//  Created by Joakim Poulsen on 07/02/2020.
+//  Copyright © 2020 Joakim Poulsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var btn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Back(_ sender: Any) {
+   
+    }
+    
+    }
